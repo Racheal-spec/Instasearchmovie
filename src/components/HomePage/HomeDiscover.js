3d09cee1608daa48e1130971dd14451e021c8596
Racheal-spec <folorunshotosin334@gmail.com>
2021-02-09 10:02:10 -0800

    Enable search query display result on homepage

--- src/components/HomePage/HomeDiscover.js
+++ src/components/HomePage/HomeDiscover.js
@@ -9,7 +9,7 @@ useEffect(() => {
   const getMovies = async () =>{
     const Api_key = "6b00a02116b6c9fb27ad808ea1eaedbd";
     
-    const apiUrl = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${Api_key}&language=en-US&page=1&include_adult=false`);
+    const apiUrl = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${Api_key}&language=en-US&page=1&include_adult=false`);
     const data = await apiUrl.json();
     setTrends(data.results)
     console.log(data);
@@ -36,8 +36,8 @@ return(
           <i className="fas fa-star"></i>
             {trend.vote_average}
           </p>  
-          <h1>{trend.title}</h1> 
-          <p>{trend.release_date}</p>
+          <h1>{trend.title || trend.name}</h1> 
+          <p>{trend.release_date || trend.first_air_date}</p>
           <button className="trend-btn"><i className="fas fa-plus"></i>Watchlist</button>
           {/*<p>OVERVIEW:{trend.overview}</p>*/}
          </div>
--- src/components/HomePage/HomeSearch.js
+++ src/components/HomePage/HomeSearch.js
@@ -7,27 +7,35 @@ const HomeSearch = () => {
     const[query, setQuery] = useState('');
     const[searches, setSearches] = useState('');
     const[results, setResults] = useState([]);
+    const[click, setClick] = useState(false);
+
+    useEffect(()=> {
+      btnClick(); 
+    }, [])//eslint-disable-line react-hooks/exhaustive-deps
 
     const updateResults = (e) =>{
-        setSearches(e.target.value);
-         
+        setSearches(e.target.value);   
       }
       const getResults = (e) =>{
         e.preventDefault();
         setQuery(searches);
-        }
+        setSearches('');
+      }
+        const btnClick = () => {
+          setClick(!click);
+      }
  
     useEffect(() => {
       const searchMovies = async () => {
         const Api_key = "6b00a02116b6c9fb27ad808ea1eaedbd";
        
-    
         const searchUrl = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${Api_key}&language=en-US&page=1&include_adult=false&query=${query}`);
         const results = await searchUrl.json();
         console.log(results);
         setResults(results.results);
     }
     searchMovies();
+   
  }, [query])
       
     return (
@@ -44,23 +52,30 @@ const HomeSearch = () => {
         </form>
         </div>
 
-        <div className= "card-overall">
-        {results && results.filter((result) => result.poster_path).map((result) => (
-         <div className= "card" key= {result.id}>
-         <Link to={`/HomeDiscover/${result.id}`}>
-        <img  className="card--img" src = {`https://image.tmdb.org/t/p/w500${result.poster_path}`}
-         alt= 'Images' />
-         </Link>
-       <div className = "cardContent">
-       <h1> 
-       <Link to={`/HomeDiscover/${result.id}`}>{result.title}
-       </Link>
-       </h1> 
-        </div>
-      <p>RELEASE-DATE:{result.release_date}</p>
-      </div>
+        <div className={click? "card-overall" : "cancel"}>
+         {results?.length === 0 ? (
+          <h2>Opps! This movie is currently not available. Please check that the title is spelt correctly</h2>
+          ) : (
+          results&& results.filter((result) => result.poster_path).map((result) => (
+            <div className= "search-card" key= {result.id}>
+             <h2>{result.title}</h2>
+            <Link to={`/HomeDiscover/${result.id}`}>
+           <img  className="card--img" src = {`https://image.tmdb.org/t/p/w500${result.poster_path}`}
+            alt= 'Images' />
+            </Link>
+            <h4>RELEASE-DATE:{result.release_date}</h4>
+            <p>OVERVIEW: {result.overview}</p> 
+            <div className="cancel-btn">
+           <i class="far fa-times-circle" onClick={btnClick}></i>
+           </div>
+           </div>
+           ))
+    )
+         
+        } 
       
-        ))}
+
+
       </div>
         </section>
         </div>
